ae9f5c15594d3970e6d02761390420ae42d83728
Anton Kandyba <antonkandyba@gmail.com>
2022-06-15 16:27:56 +0300

    added simple text

--- js/memeController.js
+++ js/memeController.js
@@ -5,11 +5,25 @@ const gCtx = gCanvas.getContext('2d')
 
 function renderMeme() {
 	const img = new Image()
-	img.src = '/img/5.jpg'
-
-	// gCtx.fillRect(100, 100, 100, 100)
-	// gCtx.drawImage(img, 0, 0, 500, 500)
+	img.src = 'img/5.jpg'
+	// render image when it's ready
 	img.onload = () => {
 		gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+
+		renderTextLine('hello', gCanvas.width / 2, 50)
 	}
+}
+
+function renderTextLine(txt, x, y) {
+	gCtx.font = '48px roboto-regular'
+	gCtx.textAlign = 'center'
+	gCtx.lineWidth = 7
+	gCtx.fillStyle = 'white'
+	gCtx.shadowOffsetX = 3
+	gCtx.shadowOffsetY = 3
+	gCtx.shadowBlur = 7
+	gCtx.shadowColor = 'black'
+
+	gCtx.strokeText(txt, x, y)
+	gCtx.fillText(txt, x, y)
 }
